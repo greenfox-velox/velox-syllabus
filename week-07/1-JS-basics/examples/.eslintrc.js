7a331e233f6d2a620c360ba4e3f3092cb8af4055
Fejes Aniko <oranjje@gmail.com>
2016-06-29 15:59:58 +0200

    eslintrc example

---
+++ week-07/1-JS-basics/examples/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb",
+    "installedESLint": true,
+    "plugins": [
+        "react"
+    ],
+    "modules": false,
+    "rules": {
+      "no-var": "off",
+      "no-console": "off",
+      "strict": "off",
+      "vars-on-top": "off",
+    }
+};
--- week-07/1-JS-basics/examples/if.js
+++ week-07/1-JS-basics/examples/if.js
@@ -7,21 +7,23 @@ if (a === 2) {
 }
 
 if (a === 2) {
-  console.log("two");
+  console.log('two');
 } else {
-  console.log("other");
+  console.log('other');
 }
 
 if (a === 1) {
-  console.log("one");
+  console.log('one');
 } else if (a === 2) {
-  console.log("two");
+  console.log('two');
 } else {
-  console.log("a lot");
+  console.log('a lot');
 }
 
+/* eslint-disable no-empty */
 if (a === 1) {
 
 } else {
-  console.log("not one");
+  console.log('not one');
 }
+/* eslint-enable no-empty */
